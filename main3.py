be07fba60b1da3351b27c8705cb7c374b3b4c383
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-10-15 17:37:15 +0200

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,69 +1,107 @@
 import keyboard
 import time
+import random
 
-# Kígyó kezdőállapot
 kigyo_test_koordinatak = [(5, 0)]
 iranyok = {
-    "d": (0, 1),   # lefelé
-    "é": (0, -1),  # felfelé
-    "ny": (-1, 0), # balra
-    "k": (1, 0)    # jobbra
+    "d": (0, 1),   # Lefelé
+    "é": (0, -1),  # Felfelé
+    "ny": (-1, 0), # Balra
+    "k": (1, 0)    # Jobbra
 }
 
-# Utolsó irány tárolása
-utolsoirany = ["k"]  # Kezdőirány jobbra
+alma_pozicio = (2, 3)
+utolsoirany = "k"
+kigyo_sebesseg = 0.5
+mozgas_idozite = 0
+zaszlo: bool = True
 
-# Kígyó mozgásának módosítása
-def kigy_modosito(irany):
+
+
+def kigy_modosito(irany, alma_pozicio):
+    """Kígyó pozíciójának módosítása mozgás esetén"""
     x1, y1 = iranyok[irany]
     x2, y2 = kigyo_test_koordinatak[-1]
     x3, y3 = (x1 + x2, y1 + y2)
     
-    # Ellenőrizzük, hogy a kígyó nem ütközik-e magába vagy nem megy ki a pályáról
+    # Ütközés ellenőrzés
     if (x3, y3) not in kigyo_test_koordinatak[1:] and 0 <= x3 <= 9 and 0 <= y3 <= 9:
         kigyo_test_koordinatak.append((x3, y3))
-        kigyo_test_koordinatak.pop(0)  # Töröljük az utolsó elemet, hogy a kígyó hossza állandó maradjon
         return True
     return False
 
-# Mozgási sebesség beállítása
-mozgasi_sebesseg = 1  # Másodpercben mért időköz a lépések között
-utolso_mozgas_ideje = time.time()
+def alma_modosito():
+    """A véletlenszerű alma legenerálása"""
+    uj_kigyo_test_koordinatak = []
 
-# Fő ciklus
-zaszlo = True
-while zaszlo:
-    # Tábla inicializálása
+    for i in kigyo_test_koordinatak:
+        uj_kigyo_test_koordinatak.extend([(i[0], i[1] + 1), (i[0], i[1] - 1), (i[0] + 1, i[1]), (i[0] - 1, i[1]), (i[0], i[1])])
+    
+    osszes_tabla_koordinata = []
+    for x in range(10):
+        for y in range(10):
+            osszes_tabla_koordinata.append((x, y))
+
+    potencialis_alma_pozicio = list(set(osszes_tabla_koordinata) - set(uj_kigyo_test_koordinatak))
+    uj_alma_pozicio = random.choice(potencialis_alma_pozicio)
+    return uj_alma_pozicio
+
+def alma_eves_kezelo(kigyo_test_koordinatak, alma_pozicio):
+    """Az almaevés kezelése és a kígyó koordinátáinak frissítése"""
+    if kigyo_test_koordinatak[-1] == alma_pozicio:
+        alma_pozicio = alma_modosito()  # Új alma generálása
+    else:
+        kigyo_test_koordinatak.pop(0)  # Csak akkor törlünk, ha nem evett almát
+    
+    return alma_pozicio
+
+def tabla_kezelo():
+    """A játéktábla megjelenítése a kígyó és a kigyo_test_koordinátájának alapján"""
     tabla = [["." for _ in range(10)] for _ in range(10)]
     
-    # Kígyó megjelenítése a táblán
     for i in kigyo_test_koordinatak:
         x, y = i
         tabla[y][x] = "X"
+
+    xx, yy = alma_pozicio
+    tabla[yy][xx] = "O"
     
-    # Tábla kiíratása
-    print("\033[H\033[J", end="")  # Képernyő törlése
+    print("\033[H\033[J", end="")
     [print(*i) for i in tabla]
+    print(kigyo_test_koordinatak)  
+
+
+def billentyu_kezelo(utolsoirany):
+    """A billentyűk lenyomásának kezelése"""
+    if keyboard.is_pressed("left") and utolsoirany != "k":  # Balra csak akkor, ha nem jobbra ment
+        utolsoirany = "ny"
+    elif keyboard.is_pressed("right") and utolsoirany != "ny":  # Jobbra csak akkor, ha nem balra ment
+        utolsoirany = "k"
+    elif keyboard.is_pressed("up") and utolsoirany != "d":  # Felfelé csak akkor, ha nem lefelé ment
+        utolsoirany = "é"
+    elif keyboard.is_pressed("down") and utolsoirany != "é":  # Lefelé csak akkor, ha nem felfelé ment
+        utolsoirany = "d"
     
-    print(kigyo_test_koordinatak)  # Kígyó jelenlegi helye
+    return utolsoirany
+
+
+while zaszlo:
+    tabla_kezelo()
     
-    # Nyíl billentyűk kezelése a keyboard modul segítségével
-    if keyboard.is_pressed("left") and utolsoirany[-1] != "k":  # Balra csak akkor, ha nem jobbra ment
-        utolsoirany.append("ny")
-    elif keyboard.is_pressed("right") and utolsoirany[-1] != "ny":  # Jobbra csak akkor, ha nem balra ment
-        utolsoirany.append("k")
-    elif keyboard.is_pressed("up") and utolsoirany[-1] != "d":  # Felfelé csak akkor, ha nem lefelé ment
-        utolsoirany.append("é")
-    elif keyboard.is_pressed("down") and utolsoirany[-1] != "é":  # Lefelé csak akkor, ha nem felfelé ment
-        utolsoirany.append("d")
+    utolsoirany = billentyu_kezelo(utolsoirany)
     
-    # Ellenőrizzük, hogy elérkeztünk-e a következő mozgási időponthoz
-    if time.time() - utolso_mozgas_ideje > mozgasi_sebesseg:
-        # Mozgatjuk a kígyót az utolsó irány alapján
-        irany = utolsoirany[-1]
-        if not kigy_modosito(irany):
+    irany = utolsoirany
+
+    aktualis_ido = time.time()
+    if aktualis_ido - mozgas_idozite >= kigyo_sebesseg:
+        mozgas_tortent = kigy_modosito(irany, alma_pozicio)
+        if not mozgas_tortent:
             print("A kígyó ütközött! Játék vége.")
             zaszlo = False
-        
-        # Frissítjük az utolsó mozgás időpontját
-        utolso_mozgas_ideje = time.time()
+        mozgas_idozite = aktualis_ido
+
+        # Az almaevés ellenőrzése és kezelése
+        alma_pozicio = alma_eves_kezelo(kigyo_test_koordinatak, alma_pozicio)
+
+    # Csökkentjük a késleltetést, hogy az irányváltás gyorsabb legyen
+    time.sleep(0.05)
